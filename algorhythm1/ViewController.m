f80a0fbe8fc28e4229a4ee0f57ac9836aa9d854d
will <will@willlam.net>
2015-12-26 19:00:57 -0500

    added button to go over basics where button, upon being pressed changed the background to orange

--- algorhythm1/ViewController.m
+++ algorhythm1/ViewController.m
@@ -16,12 +16,18 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+   [self.aButton setTitle:@"Press me!" forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)buttonPressed:(UIButton *)sender {
+  
+  self.view.backgroundColor = [UIColor orangeColor];
+}
+
 
 @end
